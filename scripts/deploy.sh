765f188633addd154a5c68781033df417046dc07
Arda Karaduman <akaraduman@gmail.com>
2016-12-14 15:29:22 +0900

    fix debarch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 eval $1
-
+echo $1
+echo $DEBARCH
 echo $TRAVIS_TAG
 
 RELEASE=main
@@ -11,7 +12,7 @@ ls
 ls ..
 
 deb-s3 upload --bucket deb-repo-coze \
-       -a $DEB_ARCH --lock --no-fail-if-exists --preserve-versions \
+       -a $DEBARCH --lock --no-fail-if-exists --preserve-versions \
        --access-key-id=$AWS_ACCESS_KEY \
        --secret-access-key=$AWS_SECRET_KEY \
        --sign=$RELEASE_KEY_ID \
