07375ef5830b695ddb4affc37b4dc56515638f1e
Arda Karaduman <akaraduman@gmail.com>
2016-12-14 15:49:32 +0900

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,12 @@
-eval $1
+OIFS=$IFS
+IFS=','
+vars=$1
+for var in $vars
+do
+    eval $var
+done
+
+IFS=$OIFS
 
 go build -o files/usr/bin/helloworld
 dpkg-buildpackage -a$DEBARCH -tlinux -B -us -uc
